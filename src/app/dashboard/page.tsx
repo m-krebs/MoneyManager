053b1c36f71936005117303c43e31f15a5193f9a
Michael Krebs <80811771+m-krebs@users.noreply.github.com>
2023-04-23 00:07:59 +0200

    :art: update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
+import Button from "@/components/ui/Button";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+  return <div>page<Button>abc</Button></div>;
 };
 
 export default page;
