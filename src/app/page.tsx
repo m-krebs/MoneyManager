6f12324aa0921f51d2c4f0e759281c717e005f94
Michael Krebs <80811771+m-krebs@users.noreply.github.com>
2023-04-26 23:37:07 +0200

    :lipstick: update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Button from "@/components/ui/Button";
+import Navbar from "@/components/navbar/Navbar";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
   return (
     <>
-      <Button>Hello</Button>
-      <Button variant="cancel">Cancel</Button>
+    <Navbar></Navbar>
+      <div className="flex flex-col gap-5 max-w-7xl mx-auto items-center relative sm:items-start">
+        <div>
+          <h1 className="text-6xl max-w-lg">
+            <b>Your personal Finance Manager</b>
+          </h1>
+        </div>
+        <div className="max-w-lg">
+          Never lose track of your finances again! Start using this awesome
+          Finance Manager today, just by pressing the button below.
+        </div>
+        <div className="flex">
+          <Link href={"/login"}>
+            <Button className="w-fit" variant={"call2action"}>
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
