426e34d6ebc5cf253476847a1721d56ba2852e74
Michael Krebs <80811771+m-krebs@users.noreply.github.com>
2023-05-09 23:36:08 +0200

    :lipstick: update tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        bgcolor: "#02182c",
+        primary: "#71fead",
+        secondary: "#002647",
+        "secondary-accent": "#001e39",
+        accent: "#437e60",
+        text: "#ffffff",
+        black: "#000000",
       },
     },
   },
   plugins: [],
-}
+};
