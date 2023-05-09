18b2b76a835580c1b2624b1970fe0260acc35124
Michael Krebs <80811771+m-krebs@users.noreply.github.com>
2023-05-09 23:37:10 +0200

    :lipstick: update navbar L&F

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,28 @@
 import { FC } from "react";
 import Button from "../ui/Button";
 import Link from "next/link";
-
+import Image from "next/image";
 
 interface NavbarProps {}
 
 const navbar: FC<NavbarProps> = ({}) => {
-  return <div className="flex flex-row justify-between items-center">
-    <nav>
-      <div>logo</div>
-      <Link href={"/login"}><Button variant={"call2action"}>Sign Up</Button></Link> 
-    </nav> 
-  </div>; 
+  return (
+    <div className="justify-between items-center max-w-7xl mx-auto my-3">
+      <nav className="flex flex-row">
+        <div>
+          <Image
+            src="https://picsum.photos/100/50"
+            alt="Logo"
+            width={100}
+            height={50}
+          />
+        </div>
+        <Link className="float-right mr-0" href={"/login"}>
+          <Button variant={"call2action"}>Sign Up</Button>
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default navbar;
