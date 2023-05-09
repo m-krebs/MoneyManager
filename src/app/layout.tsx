6896fd5226d9e7b7991a9bf5295c504ca5ec425a
Michael Krebs <80811771+m-krebs@users.noreply.github.com>
2023-05-09 23:36:34 +0200

    :lipstick: change general background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className='bg-bgcolor'>{children}</body>
     </html>
   )
 }
